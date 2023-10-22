53f908de4bbafcb3b30da0b37efdcf2dfc8bc5d0
moshe maman <moshework1985@gmail.com>
2023-10-22 07:50:51 +0300

    Add project files.

---
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,10 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Content { get; set; }
+    }
+}
