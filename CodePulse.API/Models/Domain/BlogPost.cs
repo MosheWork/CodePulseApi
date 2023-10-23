47360f1e322a565b9610693dd565591d4d45b035
moshe maman <moshework1985@gmail.com>
2023-10-23 06:57:36 +0300

    1

--- CodePulse.API/Models/Domain/BlogPost.cs
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -6,5 +6,9 @@
         public string Title { get; set; }
         public string ShortDescription { get; set; }
         public string Content { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedData { get; set; }
+        public bool IsVisible { get; set;}
     }
 }
